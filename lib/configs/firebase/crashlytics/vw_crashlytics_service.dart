a2b7c0731b050342c242ab69ce1eafa44665e6ce
Alan Pinho <alan@yardim.com.br>
2023-09-23 01:56:20 -0300

    adicionando serviço do crashlytics

---
+++ lib/configs/firebase/crashlytics/vw_crashlytics_service.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
+
+class VWCrashlyticsService {
+  final FirebaseCrashlytics _crashlytics = FirebaseCrashlytics.instance;
+
+  VWCrashlyticsService._();
+
+  static VWCrashlyticsService get i => VWCrashlyticsService._();
+
+  void recordError(exception, {StackTrace? stack, bool fatal = false}) {
+    _crashlytics.recordError(
+      exception,
+      stack,
+      fatal: fatal,
+      printDetails: false,
+    );
+  }
+
+  void setUserIdentifier(String userId) {
+    _crashlytics.setUserIdentifier(userId);
+  }
+
+  void recordFlutterError(FlutterErrorDetails error) {
+    _crashlytics.recordFlutterError(error);
+  }
+
+  void recordFlutterFatalError(FlutterErrorDetails error) {
+    _crashlytics.recordFlutterFatalError(error);
+  }
+
+  void crashlyticsLog(String message) {
+    _crashlytics.log(message);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'dart:async';
+import 'dart:developer';
+import 'dart:isolate';
+
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localization/flutter_localization.dart';
 import 'package:get_it/get_it.dart';
 import 'package:intl/intl.dart';
+import 'package:logging/logging.dart';
+import 'package:valowiki/configs/firebase/crashlytics/vw_crashlytics_service.dart';
 import 'package:valowiki/configs/themes/theme.dart';
 import 'package:valowiki/dependencies_injection.dart';
 import 'package:valowiki/env.dart';
@@ -12,26 +17,53 @@ import 'package:valowiki/pages/splash/splash_page.dart';
 import 'package:valowiki/routes.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // final Logger log = Logger('main');
+  await runZonedGuarded(() async {
+    Logger.root.level = kDebugMode ? Level.FINE : Level.INFO;
+    Logger.root.onRecord.listen((record) {
+      log('[${record.level.name}] [${record.time}]: ${record.message}');
 
-  Intl.defaultLocale = 'pt_BR';
-  await initDependenciesInjection();
+      if (record.error != null) {
+        VWCrashlyticsService.i.recordError(
+          record.error,
+          stack: record.stackTrace,
+        );
+      }
+    });
 
-  final env = GetIt.I.get<EnvironmentConfig>();
+    WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(options: env.firebaseOptions);
+    Intl.defaultLocale = 'pt_BR';
 
-  FlutterError.onError = (errorDetails) {
-    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
-  };
+    await initDependenciesInjection();
+    final env = GetIt.I.get<EnvironmentConfig>();
+    await Firebase.initializeApp(options: env.firebaseOptions);
 
-  PlatformDispatcher.instance.onError = (error, stack) {
-    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-    return true;
-  };
+    FlutterError.onError = (errorDetails) {
+      Logger('FlutterError').severe(
+        errorDetails.exception,
+        errorDetails,
+        errorDetails.stack,
+      );
+    };
 
-  runApp(const MyApp());
+    PlatformDispatcher.instance.onError = (error, stack) {
+      Logger('PlatformDispatcher').severe(error.toString(), error, stack);
+      return true;
+    };
+
+    Isolate.current.addErrorListener(RawReceivePort((pair) async {
+      final List<dynamic> errorAndStacktrace = pair;
+      Logger('Isolate').severe(
+        errorAndStacktrace.first.toString(),
+        errorAndStacktrace.last,
+        errorAndStacktrace.last,
+      );
+    }).sendPort);
+
+    runApp(const MyApp());
+  }, (error, stack) {
+    Logger('runZonedGuarded').severe(error.toString(), error, stack);
+  });
 }
 
 class MyApp extends StatelessWidget {
